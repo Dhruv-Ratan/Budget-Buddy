b476b53299ef176bf8f2ea5d531c70f0e747d781
BhavyaManvitha-Chinni <161038796+BhavyaManvitha-Chinni@users.noreply.github.com>
2026-02-09 11:10:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,16 +3,17 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSBDRVutnL8T0axdo5m2jd1HXEIDOs80k",
-    authDomain: "budget-buddy-a5b41.firebaseapp.com",
-    projectId: "budget-buddy-a5b41",
-    storageBucket: "budget-buddy-a5b41.appspot.com",
-    messagingSenderId: "162590120956",
-    appId: "1:162590120956:web:6ed2c64fe818ad899e7079",
-    measurementId: "G-HT6X8QGSP4"
+    apiKey: "APIKEY",
+    authDomain: "domain",
+    projectId: "id",
+    storageBucket: "bucket",
+    messagingSenderId: "id",
+    appId: "id",
+    measurementId: "id"
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 export { database };
+
